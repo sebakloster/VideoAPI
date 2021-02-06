51f10abfdc26cca311b345bea467a512f92fe70c
Seba Kloster <sebikloster@yahoo.com.ar>
2021-02-06 10:35:27 -0300

    Implementing errors hanlder

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const moviesApi = require("./routes/movies");
 
+const { logErrors, errorHandler } = require("./utils/middleware/errorHandlers");
+
 const { config } = require("./config/index");
 app.use(express.json());
 moviesApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
   console.log(`Listening on port: ${config.port}`);
