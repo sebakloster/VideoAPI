313c47ff00a8641a0d6ce6d1895750f9eac49041
Seba Kloster <sebikloster@yahoo.com.ar>
2021-04-26 10:41:03 -0300

    Agregando OAuth Para hacer la autorizacion con google

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,10 @@ const ApiKeysServices = require("../services/apiKeys");
 const UsersService = require("../services/users");
 const validationHandler = require("../utils/middleware/validationHandler");
 const { config } = require("../config");
-const { createUserSchema } = require("../utils/schemas/users");
+const {
+  createUserSchema,
+  createProviderUserSchema,
+} = require("../utils/schemas/users");
 
 // Basic strategy
 
@@ -76,6 +79,38 @@ function authApi(app) {
           data: createdUserId,
           message: "User created",
         });
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
+  router.post(
+    "/sign-provider",
+    validationHandler(createProviderUserSchema),
+    async function (req, res, next) {
+      const { body } = req;
+      const { apiKeyToken, ...user } = body;
+      if (!apiKeyToken) {
+        next(boom.unauthorized("apiKeyToken is required"));
+      }
+
+      try {
+        const queriedUser = await usersService.getOrCreateUser({ user });
+        const apiKey = await apiKeysServices.getApiKey({ token: apiKeyToken });
+        if (!apiKey) {
+          next(boom.unauthorized());
+        }
+        const { _id: id, name, email } = queriedUser;
+        const payload = {
+          sub: id,
+          name,
+          email,
+          scopes: apiKey.scopes,
+        };
+        const token = jwt.sign(payload, config.authJwtSecret, {
+          expiresIn: "15m",
+        });
+        return res.status(200).json({ token, user: { id, name, email } });
       } catch (error) {
         next(error);
       }
